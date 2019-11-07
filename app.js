aeeceddddd133a354b7278f614d494a14729bea6
Alec <a_riddick@fanshaweonline.ca>
2019-11-07 12:29:16 -0500

    Week 9: Error Page WORKING

--- app.js
+++ app.js
@@ -8,10 +8,24 @@ const port = process.env.PORT || 3000;
 const app = express();
 
 app.use(express.static(path.join(__dirname, '/public')));
+
 app.set('view engine', 'hbs');
 app.set('views', path.join(__dirname, 'views'));
 
 app.use('/', require('./routes/index'));
+
+app.use((req, res, next) => {
+    var err = new Error('Not Found');
+    err.status = 404;
+    err.customMessage = "Something terrible has happened! Your page has gone missing!"
+    //err.customImage = images/jurassic_park-MSMediaKeyNeededEvent.gif
+
+    next(err);
+})
+
+app.use((err, req, res, next) => {
+    res.render('error', {data: err, layout: 'errorPage' })
+})
 
 app.listen(port, () => {
     console.log(`app is running on ${port}`);
